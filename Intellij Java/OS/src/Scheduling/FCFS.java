144e6e73bd87f7a795147b5804947f1e7217b0dd
greenday1234 <ing3616062@naver.com>
2023-05-05 20:31:45 +0900

    OS FCFS

--- Intellij Java/OS/src/Scheduling/FCFS.java
+++ Intellij Java/OS/src/Scheduling/FCFS.java
@@ -1,4 +1,102 @@
 package Scheduling;
 
+import java.util.*;
+
 public class FCFS {
+
+    public FCFS(Sorce[] sorce){
+        //새로운 객체 배열에 도착시간 정렬
+        Sorce[] new_sorce = new Sorce[sorce.length];
+        for(int i=0; i<sorce.length; i++){
+            new_sorce[i] = new Sorce(sorce[i].getID(), sorce[i].getArrival(), sorce[i].getService(), sorce[i].getPriority());
+        }
+        Arrays.sort(new_sorce, (a, b) -> a.getArrival() - b.getArrival());
+
+        System.out.println("FCFS Scheduling");
+
+        //간트 차트
+        System.out.println("간트 차트");
+        System.out.print("[ ");
+        for(int k=0; k<new_sorce.length; k++) {
+            for (int i = 0; i < new_sorce[k].getService(); i++) {
+                System.out.print(new_sorce[k].getID() + " ");
+            }
+        }
+        System.out.print(" ]\n");
+
+        //각 프로세스별 평균 대기 시간
+        int[] aAWT = new int[new_sorce.length];
+        int num = 0;
+
+        for(int i=0; i<new_sorce.length; i++){
+            for(int k=0; k< i; k++){
+                num += new_sorce[k].getService();
+            }
+            aAWT[i] = num-new_sorce[i].getArrival();
+            num = 0;
+        }
+        double AWT = 0;
+        for(int i=0; i< new_sorce.length; i++){
+            AWT += aAWT[i];
+        }
+        AWT = AWT/ new_sorce.length;
+        System.out.println("각  프로세스별 평균 대기 시간");
+        System.out.println(AWT);
+
+        //각 프로세스별 응답시간
+        System.out.println("각 프로세스별 응답시간");
+        int[] aACT = new int[new_sorce.length];
+
+        for(int i=0; i<new_sorce.length; i++){
+            for(int k=0; k<i; k++){
+                num += new_sorce[k].getService();
+            }
+            if(i == 0){
+                aACT[i] = new_sorce[i].getAction();
+                num = 0;
+            }
+            else {
+                aACT[i] = num + new_sorce[i].getAction();
+                num = 0;
+            }
+            System.out.print(new_sorce[i].getID() + " : " + aACT[i] + "\n");
+        }
+
+        //평균 응답 시간
+        double ACT = 0;
+        for(int i=0; i<new_sorce.length; i++){
+            ACT += aACT[i];
+        }
+        ACT = ACT/new_sorce.length;
+        System.out.println("평균 응답 시간");
+        System.out.println(ACT);
+
+        //각 프로세스별 반환 시간
+        System.out.println("각 프로세스별 반환 시간");
+        int[] aATT = new int[new_sorce.length];
+
+        for(int i=0; i<new_sorce.length; i++){
+            for(int k=0; k<=i; k++){
+                num += new_sorce[k].getService();
+            }
+            if(i == 0){
+                aATT[i] = new_sorce[i].getService() - new_sorce[i].getArrival();
+                num = 0;
+            }
+            else {
+                aATT[i] = num - new_sorce[i].getArrival();
+                num = 0;
+            }
+            System.out.print(new_sorce[i].getID() + " : " + aATT[i] + "\n");
+        }
+
+        //평균 반환 시간
+        double ATT = 0;
+        for(int i=0; i<new_sorce.length; i++){
+            ATT += aATT[i];
+        }
+        ATT = ATT/new_sorce.length;
+        System.out.println("평균 반환 시간");
+        System.out.println(ATT);
+    }
 }
--- Intellij Java/OS/src/Scheduling/Scheduling.java
+++ Intellij Java/OS/src/Scheduling/Scheduling.java
@@ -1,9 +1,67 @@
 package Scheduling;
 
 import java.util.*;
-public class Scheduling {
+import java.io.*;
 
-    public static void main(String args[]){
-        System.out.println("hellow world");
+class Sorce {
+    String ID;
+    int Arrival;
+    int Service;
+    int Priority;
+    int Time;
+    int Action = 1;
+    public Sorce(String ID, int Arrival, int Service, int Priority){
+        this.ID = ID;
+        this.Arrival = Arrival;
+        this.Service = Service;
+        this.Priority = Priority;
+    };
+    public void setTime(int Time){ this.Time = Time; }
+    public String getID(){ return ID; }
+    public int getArrival(){ return Arrival; }
+    public int getService(){ return Service; }
+    public int getPriority(){ return Priority; }
+    public int getAction(){ return Action; }
+}
+
+public class Scheduling {
+    public static void main(String args[]) throws IOException {
+        //데이터 값 변수
+        int ProcessCount;
+        String str;
+        String ID;
+        int Arrival;
+        int Service;
+        int Priority;
+        int Time;
+        Scanner scanner = new Scanner(new File("./Input.txt"));
+        //파일에서 데이터 읽어서 객체 배열에 값 넣기
+        ProcessCount = Integer.parseInt(scanner.nextLine());
+        Sorce[] sorce = new Sorce[ProcessCount];
+
+        for (int i = 0; i < ProcessCount; i++) {
+            str = scanner.nextLine();
+            String[] arr = str.split(" ");
+            ID = arr[0];
+            Arrival = Integer.parseInt(arr[1]);
+            Service = Integer.parseInt(arr[2]);
+            Priority = Integer.parseInt(arr[3]);
+            sorce[i] = new Sorce(ID, Arrival, Service, Priority);
+        }
+
+        Time = Integer.parseInt(scanner.nextLine());
+        for(int i=0; i<ProcessCount; i++){
+            sorce[i].setTime(Time);
+        }
+
+        FCFS fcfs = new FCFS(sorce);
+        //SJF sjf = new SJF();
+        //Non_Priority np = new Non_Priority();
+        //Priority pri = new Priority();
+        //RR rr = new RR();
+        //SRT srt = new SRT();
+        //HRN hrn = new HRN();
+
+
     }
 }
